1de241cc0910828324509217a81b3698bc2f72d1
Philip Edin <philip.edin@gmail.com>
2026-01-03 16:36:47 +0100

    Set nvim indentation defaults

--- init.lua
+++ init.lua
@@ -84,6 +84,14 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+
+-- === Indentation: global defaults ===
+vim.opt.expandtab = true      -- spaces, not tabs
+vim.opt.tabstop = 2           -- how wide a tab looks
+vim.opt.shiftwidth = 2        -- >> and <<
+vim.opt.softtabstop = 2       -- <Tab> inserts 2 spaces
+vim.opt.smartindent = true
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
